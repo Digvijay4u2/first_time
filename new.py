3a8c73949770178bff97bf887b325aa3ce748bb7
digvijay4u2 <digvijay4u2@gmail.com>
2021-02-08 17:47:48 +0530

    first time commit

---
+++ new.py
@@ -0,0 +1,9 @@
+
+print(2+3)
+n1=int(input("Enter number 1:"))
+n2=int(input("Enter number 2:"))
+if n1+n2>30:
+    print(n1+n2)
+    print(str(n1+n2)+'  is greater than ' +str(30) )
+else:
+    print("smaller")
